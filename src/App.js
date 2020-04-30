bf49e6d79761bc58cb14d48bd00b30262e08db5c
ice <1072702399@qq.com>
2020-04-30 15:36:12 +0800

    user

--- src/App.js
+++ src/App.js
@@ -29,10 +29,26 @@ testone(){
     const elements=[];
     this.state.link.forEach((item,index)=>{
       if(item.type==this.state.type){//eslint-disable-line
-      console.log(item.type,'item.type',this.state.type,item.name,this.state.movietype);
+ //     console.log(item.type,'item.type',this.state.type,item.name,this.state.movietype);
+      var backlink='./'+item.pic;
+      console.log(backlink,'backlink');
+      var background={
+    display: 'block',
+    font: "200 18px/40px 'Microsoft YaHei'",
+    height: '200px',
+    width: '100%',
+    float: 'left',
+    color: '#fff',
+    cursor: 'pointer',
+    border: '1px solid #2fa4e7',
+    background: "url(" + require('./'+item.pic) + ") center center / cover no-repeat",
+};
+//let screenWidth = Dimensions.get('window').width;
       elements.push(
                  <li>
-                    <iframe id="video_iframe" src={item.link}></iframe>
+                    <div>
+                    <button class="video_iframe" style={background}> ▶&nbsp;播放</button>     
+                    </div>
                     <div class="movies-info">
                         <span class="movie-name">视频类{item.name}</span>
                     </div>
@@ -41,12 +57,13 @@ testone(){
     });
     console.log(this.state.movietype);
     return(
-      <div class={this.state.movietype}>
+      <div class={this.state.movietype} style={{display: 'block',}}>
         <br />
         <br />
             <ul>
         {elements}
             </ul>
+
       </div>
     )
 }
@@ -57,6 +74,51 @@ this.testone();
   render() {
   return (
 <div>
+<div class="site-nav clearfix">
+    <div class="w">
+        <div class="site-nav-logo">
+            <a href="#" class="logo"><img src={require("./images/logo.jpg")} alt=""/>Video</a>
+        </div>
+<div class="site-nav-search">
+            <form action="" method="get">
+                <input type="text" placeholder="请输入电影名称"/>
+                <button type="submit">搜索</button>
+            </form>
+        </div>
+        <div class="site-nav-r">
+            <ul>
+                <li><a href="index.html">电影</a></li>
+                <li><a href="login.html">登录</a></li>
+                <li><a href="register.html">注册</a></li>
+                <li><a href="index.html">退出</a></li>
+                <li><a href="user.html">会员</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+            <br />
+            <br />
+            <br />
+            <br />
+<div class="main" >
+    <div class="w">
+        <div class="main-slider">
+            <ul>
+                <li class="first"></li>
+                <li><a href="play.html?id=1"><span>视频1</span><span>简介1</span></a></li>
+                <li><a href="play.html?id=2"><span>视频2</span><span>简介2</span></a></li>
+                <li><a href="play.html?id=3"><span>视频3</span><span>简介3</span></a></li>
+                <li><a href="play.html?id=4"><span>视频4</span><span>简介4</span></a></li>
+                <li><a href="play.html?id=5"><span>视频5</span><span>简介5</span></a></li>
+                <li><a href="play.html?id=6"><span>视频6</span><span>简介6</span></a></li>
+                <li><a href="play.html?id=7"><span>视频7</span><span>简介7</span></a></li>
+                <li class="last"></li>
+            </ul>
+            <a href="javascript:void(0);" class="arrow-l"> ◀</a>
+            <a href="javascript:void(0);" class="arrow-r"> ▶ </a>
+        </div>
+    </div>
+</div>
  <div class="movies clearfix" >
     <div class="w">
         <div class="movies-type">
@@ -70,6 +132,11 @@ this.testone();
             </ul>
         </div>
         {this.testone()}
+<div class="site-nav2" >
+        <div style={{height:'200px'}}>
+            <iframe  src="images/1.mp4" style={{width:'100%',marginleft: '50%',background:'#fff',}}></iframe>
+        </div>
+</div>
     </div>
 </div>
 </div>
